98bbfbaf1cdce42595af60cc590ea4a3766c4583
Mohamed Allam <mohamedallam.tu@gmail.com>
2022-03-04 06:52:21 +0100

    Add all indexedDB features

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,4 +1,5 @@
 import { openDB } from 'idb';
+import { header } from './header';
 
 const dbName = "jate";
 const storeName = "jate";
@@ -20,11 +21,19 @@ const initdb = async () =>
 export const putDb = async (content) => {
   const db = await openDB(dbName);
   const store = db.transaction(storeName, 'readwrite').objectStore(storeName);
-  const key = await store.put({content, id: contentKey})
+  const key = await store.put({ content, id: contentKey })
   console.log('Inserted content with key ', key, 'data: ', content)
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+  const db = await openDB(dbName);
+  const store = db.transaction(storeName, 'readwrite').objectStore(storeName);
+  const data = await store.get(contentKey);
+  if (!data) {
+    return header
+  }
+  console.log('Got Data ', contentKey, 'data: ', data.content)
+  return data.content;
+}
 initdb();
